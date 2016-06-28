461b4ec11d2f09f26e2bc9db5b29919e2c72f5b3
Jada <jwashin7@terpmail.umd.edu>
2016-06-28 10:25:42 -0400

    Cleaning..

--- pmportal/src/main/java/com/sgt/pmportal/domain/Release.java
+++ pmportal/src/main/java/com/sgt/pmportal/domain/Release.java
@@ -3,29 +3,44 @@ package com.sgt.pmportal.domain;
 import java.net.URI;
 import org.joda.time.DateTime;
 
+/**
+ * A release or version of a Jira project
+ * 
+ * @author Aman Mital
+ * @author Jada Washington
+ *
+ */
 public class Release{
 	protected String name;
 	protected Long id;
-	protected DateTime release;
+	protected DateTime releaseDate;
 	protected URI uri;
-	
+
+	/**
+	 * Constructor for a Release object
+	 * 
+	 * @param versionName
+	 * @param versionId
+	 * @param versionRelease
+	 * @param versionUri
+	 */
 	public Release(String versionName, Long versionId, DateTime versionRelease, URI versionUri){    
 		name=versionName;
 		id=versionId;
-		release=versionRelease;
+		releaseDate=versionRelease;
 		uri=versionUri;
 	}
 
-public String getName(){
-	return name;
-}
-public Long getId() {
-	return id;
-}
-public DateTime getRelease() {
-	return release;
-}
-public URI getURI(){
-	return uri;
-}
+	public String getName(){
+		return name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public DateTime getRelease() {
+		return releaseDate;
+	}
+	public URI getURI(){
+		return uri;
+	}
 }
--- pmportal/src/main/java/com/sgt/pmportal/domain/Sprint.java
+++ pmportal/src/main/java/com/sgt/pmportal/domain/Sprint.java
@@ -2,37 +2,55 @@ package com.sgt.pmportal.domain;
 
 import java.util.Date;
 
+/**
+ * This class represents a Sprint in a Jira project
+ * 
+ * @author Aman Mital
+ * @author Jada Washington
+ *
+ */
 public class Sprint {
-protected String name;
-protected String id;
-protected String state;
-protected Date startDate;
-protected Date endDate;
-protected Date completeDate;
-public Sprint(String name, String id, String state, Date startDate, Date endDate, Date completeDate) {
-	this.name = name;
-	this.id = id;
-	this.state = state;
-	this.startDate = startDate;
-	this.endDate = endDate;
-	this.completeDate = completeDate;
-}
-public String getName() {
-	return name;
-}
-public String getId() {
-	return id;
-}
-public String getState() {
-	return state;
-}
-public Date getStartDate() {
-	return startDate;
-}
-public Date getEndDate() {
-	return endDate;
-}
-public Date getCompleteDate() {
-	return completeDate;
-}
+	protected String name;
+	protected String id;
+	protected String state;
+	protected Date startDate;
+	protected Date endDate;
+	protected Date completeDate;
+
+	/**
+	 * Constructor for Sprint object
+	 * 
+	 * @param name
+	 * @param id
+	 * @param state
+	 * @param startDate
+	 * @param endDate
+	 * @param completeDate
+	 */
+	public Sprint(String name, String id, String state, Date startDate, Date endDate, Date completeDate) {
+		this.name = name;
+		this.id = id;
+		this.state = state;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.completeDate = completeDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getState() {
+		return state;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public Date getCompleteDate() {
+		return completeDate;
+	}
 }
