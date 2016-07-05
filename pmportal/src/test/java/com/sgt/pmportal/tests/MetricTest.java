02f2f2d602431bd10d3623d4e3f8de2596feb418
Aman <Aman@Aman-PC>
2016-07-05 13:19:13 -0600

    Adapted MetricTest to work with Jada's Jira

--- pmportal/src/test/java/com/sgt/pmportal/tests/MetricTest.java
+++ pmportal/src/test/java/com/sgt/pmportal/tests/MetricTest.java
@@ -18,15 +18,15 @@ import com.sgt.pmportal.services.ProjectServices;
 import com.sgt.pmportal.services.SprintServices;
 
 public class MetricTest {
-	private static final String JIRA_URL = "http://54.152.100.242/jira";
-	private static final String JIRA_ADMIN_USERNAME = "amital";
-	private static final String JIRA_ADMIN_PASSWORD = "ComPuteR90";
+	private static final String JIRA_URL = "http://52.204.130.7:8081";
+	private static final String JIRA_ADMIN_USERNAME = "JWashington@sgt-inc.com";
+	private static final String JIRA_ADMIN_PASSWORD = "Diamond2017";
 	private static String authorization;
 	private static JiraRestClient client=login();
 	ProjectServices pService=new ProjectServices(client, authorization, JIRA_URL);
 	MetricsServices  metricService=new MetricsServices(client, authorization, JIRA_URL);
 	SprintServices sprintService=new SprintServices(client, authorization, JIRA_URL);
-	JiraProject project=pService.toJiraProject(client.getProjectClient().getProject("DEV").claim(),null);
+	JiraProject project=pService.toJiraProject(client.getProjectClient().getProject("TP").claim(),null);
 	List<Sprint> sprintList=new ArrayList<Sprint>();
 	/**
 	 * logs in to JiraClient
