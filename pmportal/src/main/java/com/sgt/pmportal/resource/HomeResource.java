e556349602d006ecb6bf5395229c7c2ba3cece9b
Aman <Aman@10.0.0.24>
2016-07-07 11:17:53 -0600

    Import changes, testing as JSON object

--- pmportal/src/main/java/com/sgt/pmportal/resource/HomeResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/HomeResource.java
@@ -3,11 +3,11 @@ package com.sgt.pmportal.resource;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import javax.jws.WebService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.atlassian.jira.rest.client.JiraRestClient;
 import com.sgt.pmportal.domain.JiraProject;
@@ -21,8 +21,8 @@ public class HomeResource {
 	
 	
 	@GET
-	//@Produces(MediaType.APPLICATION_JSON)
-	@Produces("text/plain")
+	@Produces(MediaType.APPLICATION_JSON)
+	//@Produces("text/plain")
 	public String getProjects(@PathParam ("username") String username, @PathParam ("password")	String password) throws URISyntaxException{
 		String url="http://54.152.100.242/jira";
 		JiraRestClient client=GeneralServices.login(url, username, password);
