c3ad6fc30edfd87b3c74e62bc654e48f99936283
Aman <Aman@10.0.0.24>
2016-07-07 14:32:17 -0600

    Made metricResource "gettable" and started services

--- pmportal/src/main/java/com/sgt/pmportal/resource/MetricResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/MetricResource.java
@@ -9,18 +9,26 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.atlassian.jira.rest.client.JiraRestClient;
+import com.sgt.pmportal.domain.JiraProject;
 import com.sgt.pmportal.services.GeneralServices;
+import com.sgt.pmportal.services.MetricsServices;
 import com.sgt.pmportal.services.ProjectServices;
 
 @Path ("/metrics")
 public class MetricResource {
 	
 	@Path("/project/{projectKey}/{username}/{password}/{url:.+}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public String getBasicMetrics(@PathParam ("projectKey") String key, 
 			@PathParam ("username") String username, 
 			@PathParam ("password") String password, 
 			@PathParam("url") String url) throws URISyntaxException{
 		JiraRestClient client=GeneralServices.login(url, username, password);
+		String authorization=GeneralServices.encodeAuth(username, password);
+		ProjectServices projectService=new ProjectServices(client, authorization, url);
+		JiraProject project=projectService.getProjectByKey(key);
+		MetricsServices metricService=new MetricsServices(client, authorization, url);
 		String responseString="";
 		return responseString;
 	}
