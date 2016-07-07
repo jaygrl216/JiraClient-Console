ca1b81ad399a5ea99e0564d04e615e6ab79e54d3
Aman <Aman@10.0.0.24>
2016-07-07 09:17:34 -0600

    Began work on HomeResource class

--- pmportal/src/main/java/com/sgt/pmportal/resource/HomeResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/HomeResource.java
@@ -1,15 +1,31 @@
 package com.sgt.pmportal.resource;
 
+import java.net.URISyntaxException;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path ("/home")
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.sgt.pmportal.domain.JiraProject;
+import com.sgt.pmportal.services.GeneralServices;
+import com.sgt.pmportal.services.ProjectServices;
+
+@Path ("/home/{username}/{password}/{url}")
 public class HomeResource {
 
 	@GET
-	public void getProjects(){
-		
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getProjects(@PathParam ("username") String username,
+			@PathParam ("password") String password,
+			@PathParam ("url") String url) throws URISyntaxException{
+		JiraRestClient client=GeneralServices.login(url, username, password);
+		String authorization=GeneralServices.encodeAuth(username, password);
+		ProjectServices projectService=new ProjectServices(client, authorization, url);
+		List<JiraProject> projectList=projectService.getAllJiraProjects();
+		return projectList.toString();
 	}
 }
