5c8e2f6090b65a521c254cd19e2b79f13fe780c4
Aman <Aman@10.0.0.2>
2016-07-11 15:23:59 -0600

    Begun work on metrics dashboard

---
+++ pmportal/UI Content/scripts/metrics.js
@@ -0,0 +1,38 @@
+﻿var username="amital";
+var password="ComPuteR90";
+var projectKey="PMPOR";
+var baseURL="http://54.152.100.242/jira";
+var metricResource="http://localhost:8080/pmportal/rest/metrics/project/detail/"+projectKey+"/" + username + "/" + password + "/" +baseURL;
+var responseObject;
+
+$.ajax({
+url:metricResource,
+dataType:"json"
+}).fail(function( xhr, status, errorThrown ) {
+    console.log( "Error: " + errorThrown );
+    console.log( "Status: " + status );
+    console.dir( xhr );
+}).done(function(jsonObject){
+	responseObject=jsonObject;
+});
+	//graphics
+$(document).ajaxStop(function(){
+	var ctx = document.getElementById("chart");
+	var chartData = {
+			labels:["Sprint 1", "Sprint 2", "Sprint3"
+			        ],
+				datasets: [
+	        {
+	        	label:"SEA",
+	            data: responseObject.sea,
+	            backgroundColor: [
+	                "#FF6384",
+	                "#36A2EB",
+	            ]
+	        }]
+	};
+	var myLineChart = new Chart(ctx,{
+	    type: 'line',
+	    data: chartData,
+	});
+});
