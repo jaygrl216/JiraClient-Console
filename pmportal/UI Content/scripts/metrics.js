2ba6835fcf62781db9400aa534523aa49089a15e
Aman <Aman@10.0.0.7>
2016-07-15 13:06:30 -0600

    chart styling

--- pmportal/UI Content/scripts/metrics.js
+++ pmportal/UI Content/scripts/metrics.js
@@ -108,7 +108,8 @@ function drawLineGraphics(){
 			        	   data:dataArray,
 			        	   fill:false,
 			        	   backgroundColor:"#FF0000",
-						   borderColor:'rgba(0, 0, 0, 0.25)',
+						   borderColor:'rgba(0, 0, 0, 0.5)',
+						   borderDash:[5,5],
 						   lineTension:0
 			           }]
 
