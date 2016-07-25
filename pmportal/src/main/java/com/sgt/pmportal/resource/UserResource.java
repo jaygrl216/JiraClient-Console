865cc446dc8e979aa33b6ec8341c25aad15b8eeb
Aman <Aman@Aman-PC>
2016-07-25 14:51:51 -0600

    removed unused imports

--- pmportal/src/main/java/com/sgt/pmportal/resource/UserResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/UserResource.java
@@ -1,18 +1,11 @@
 package com.sgt.pmportal.resource;
 
 import java.net.URISyntaxException;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import com.atlassian.jira.rest.client.JiraRestClient;
-import com.sgt.pmportal.domain.JiraProject;
-import com.sgt.pmportal.services.GeneralServices;
-import com.sgt.pmportal.services.ProjectServices;
 
 @Path ("/user/{username}/{password}/{url:.+}")
 public class UserResource {
