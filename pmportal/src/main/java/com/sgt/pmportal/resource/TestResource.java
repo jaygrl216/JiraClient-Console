13efb733819fe0e127a580cf63a7efd85a160cb8
Aman <Aman@Aman-PC>
2016-07-25 14:59:26 -0600

    Finished (?) test class

--- pmportal/src/main/java/com/sgt/pmportal/resource/TestResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/TestResource.java
@@ -2,17 +2,28 @@ package com.sgt.pmportal.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.sgt.pmportal.services.GeneralServices;
 
 @Path ("/test/{username}/{password}/{url:.+}")
 public class TestResource {
 
-	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String testLogin(){
-		return "";
+	public String testLogin(@PathParam ("username") String username, 
+			@PathParam ("password")	String password, 
+			@PathParam ("url") String url){
+		try{
+			@SuppressWarnings("unused")
+			JiraRestClient client=GeneralServices.login(url, username, password);
+		}catch (Exception e){
+			return "Failed";
+		}
+		return "Success";
 	}
 	
 }
