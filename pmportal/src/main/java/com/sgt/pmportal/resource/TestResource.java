a9b7491d84f399c48a1cb321bad4900dd1cf5989
Aman <Aman@Aman-PC>
2016-07-26 09:28:32 -0600

    Streamline TestResource

--- pmportal/src/main/java/com/sgt/pmportal/resource/TestResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/TestResource.java
@@ -1,19 +1,15 @@
 package com.sgt.pmportal.resource;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import com.atlassian.jira.rest.client.JiraRestClient;
 import com.sgt.pmportal.services.GeneralServices;
-
 @Path ("/test/{username}/{password}/{url:.+}")
 public class TestResource {
-
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	public String testLogin(@PathParam ("username") String username, 
 			@PathParam ("password")	String password, 
 			@PathParam ("url") String url){
@@ -25,5 +21,4 @@ public class TestResource {
 		}
 		return "Success";
 	}
-	
 }
