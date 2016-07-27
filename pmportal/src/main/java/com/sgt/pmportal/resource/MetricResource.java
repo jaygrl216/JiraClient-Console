a42eb17d3cfad7096fc6b8df0a1ffb69fd9ea3cc
Aman <Aman@Aman-PC>
2016-07-27 14:24:23 -0600

    directory specifically for data

--- pmportal/src/main/java/com/sgt/pmportal/resource/MetricResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/MetricResource.java
@@ -86,7 +86,7 @@ public class MetricResource {
 	        builder = factory.newDocumentBuilder();  
 	        Document document = builder.parse( new InputSource( new StringReader( xmlString ) ) );  
 	        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-	        Result output = new StreamResult(new File("../docroot/output.xml"));
+	        Result output = new StreamResult(new File("../docroot/data/output.xml"));
 	        Source input = new DOMSource(document);
 	        transformer.transform(input, output);
 	    }catch (Exception e){  
