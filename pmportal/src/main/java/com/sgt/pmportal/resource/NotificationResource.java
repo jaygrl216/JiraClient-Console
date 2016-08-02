cb132515ab41c494095d4fb740c8f576c46dbf4f
Aman <Aman@Aman-PC>
2016-08-02 13:49:33 -0600

    blank method to write file to store users

--- pmportal/src/main/java/com/sgt/pmportal/resource/NotificationResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/NotificationResource.java
@@ -5,6 +5,11 @@ import javax.ws.rs.core.MediaType;
 
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 
@@ -14,6 +19,24 @@ public class NotificationResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void saveCredentials(JSONObject requestObject){
-		
+		String username=requestObject.getString("username");
+		String password=requestObject.getString("password");
+		 try{
+		    	String lineBreak= System.getProperty("line.separator");
+		    	File textFile;
+		    	try{
+		    		//Tomcat
+		    	textFile=new File("webapps/pmportal/data/output.xls");
+		    	}catch (Exception e){
+		    		//glassfish
+		    		textFile=new File("../docroot/data/output.xls");
+		    	}
+		    	Writer fileWriter=new BufferedWriter(new FileWriter(textFile));
+		    	fileWriter.write("SEA	EEA	Bugs");
+		    	fileWriter.write(lineBreak);
+		    	fileWriter.close();
+		    }catch (Exception e){
+		    	e.printStackTrace();
+		    }
 	}
 }
