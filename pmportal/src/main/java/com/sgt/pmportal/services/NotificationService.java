9f8ed61e1c93e7f03de2968b732cc405c199445f
Aman <Aman@Aman-PC>
2016-08-02 10:18:53 -0600

    Finished code for NotificationService

--- pmportal/src/main/java/com/sgt/pmportal/services/NotificationService.java
+++ pmportal/src/main/java/com/sgt/pmportal/services/NotificationService.java
@@ -10,38 +10,31 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import com.test.email.EmailTest;
-import com.test.email.EmailTest.SMTPAuthenticator;
-
 public class NotificationService {
-	 String  d_email = "ajmital@gmail.com",
-	            d_password = "ComPuteR90",
+//This will only work if less secure apps are permitted, otherwise must use OAuth for gmail
+	 String     d_email="ajmital@gmail.com",
+			 	d_password = "ComPuteR90",
 	            d_host = "smtp.gmail.com",
-	            d_port  = "465",
-	            m_to = "ajmital@gmail.com",
-	            m_subject = "Testing",
-	            m_text = "Hey, this is the testing email.";
+	            d_port  = "465";
 	    
-	    public EmailTest()
-	    {
+	    public NotificationService(String m_to, String m_subject, String m_text){
 	        Properties props = new Properties();
 	        props.put("mail.smtp.user", d_email);
 	        props.put("mail.smtp.host", d_host);
 	        props.put("mail.smtp.port", d_port);
 	        props.put("mail.smtp.starttls.enable","true");
 	        props.put("mail.smtp.auth", "true");
-	        //props.put("mail.smtp.debug", "true");
 	        props.put("mail.smtp.socketFactory.port", d_port);
 	        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 	        props.put("mail.smtp.socketFactory.fallback", "false");
 
-	        SecurityManager security = System.getSecurityManager();
+	        @SuppressWarnings("unused")
+			SecurityManager security = System.getSecurityManager();
 
 	        try
 	        {
 	            Authenticator auth = new SMTPAuthenticator();
 	            Session session = Session.getInstance(props, auth);
-	            //session.setDebug(true);
 
 	            MimeMessage msg = new MimeMessage(session);
 	            msg.setText(m_text);
@@ -55,15 +48,9 @@ public class NotificationService {
 	            mex.printStackTrace();
 	        } 
 	    }
-	    
-	    public static void main(String[] args)
-	    {
-	        EmailTest blah = new EmailTest();
-	    }
 
 	    private class SMTPAuthenticator extends javax.mail.Authenticator{
-	        public PasswordAuthentication getPasswordAuthentication()
-	        {
+	        public PasswordAuthentication getPasswordAuthentication(){
 	            return new PasswordAuthentication(d_email, d_password);
 	        }
 	    }
