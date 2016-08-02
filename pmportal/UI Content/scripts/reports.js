765d4416b7820ae8308f7a67d83e56e8be3b22a5
Jada <jwashin7@terpmail.umd.edu>
2016-08-02 13:46:17 -0400

    took out header b/c it ruined layout

--- pmportal/UI Content/scripts/reports.js
+++ pmportal/UI Content/scripts/reports.js
@@ -71,11 +71,6 @@ $(document).ajaxStop(function () {
 
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-        header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
         contentHeight: 'auto'
     });
 })
