4df769859486dbfff3c3b31bed7f10a2d068b8a4
Aman <Aman@Aman-PC>
2016-08-02 13:38:21 -0600

    Beginning work on NotificationResource which saves credentials

--- pmportal/src/main/java/com/sgt/pmportal/resource/NotificationResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/NotificationResource.java
@@ -1,5 +1,19 @@
 package com.sgt.pmportal.resource;
 
-public class NotificationResource {
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 
+import org.json.JSONObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+
+
+@Path("/notification")
+public class NotificationResource {
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void saveCredentials(JSONObject requestObject){
+		
+	}
 }
