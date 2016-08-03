42237cf563800c55b2670307f1cd029535966808
Aman <Aman@Aman-PC>
2016-08-03 14:41:29 -0600

    Added retrieval of metrics for all projects for all users, now adding bounds

--- pmportal/src/main/java/com/sgt/pmportal/services/ScheduleService.java
+++ pmportal/src/main/java/com/sgt/pmportal/services/ScheduleService.java
@@ -1,19 +1,46 @@
 package com.sgt.pmportal.services;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.sgt.pmportal.domain.JiraProject;
+
 public class ScheduleService {
 
-	public void checkMetrics(){
-		
-
+	//This could literally take an hour. This method should only be run on a weekly basis, if that
+	public static void checkMetrics() throws IOException, URISyntaxException, ParseException{
+		JSONArray userArray=getCredentials().getJSONArray("users");
+		for (int i=0; i<userArray.length(); i++){
+			JSONObject user=userArray.getJSONObject(i);
+			String username=user.getString("username");
+			String password=user.getString("password");
+			String url=user.getString("url");
+			JiraRestClient client=GeneralServices.login(url, username, password);
+			String authorization=GeneralServices.encodeAuth(username, password);
+			ProjectServices projectService=new ProjectServices(client, authorization, url);
+			List<JiraProject> projectList=projectService.getAllJiraProjects();
+			MetricsServices metricService=new MetricsServices(client, authorization, url);
+			for (JiraProject project:projectList){
+				String key=project.getKey();
+				String name=project.getName();
+				Double sea=metricService.calculateProjectSEA(project, null);
+				Double eea=metricService.calculateProjectEEA(project, null);
+				Long bugs=metricService.calculateBugs(key);
+				Double progress=metricService.calculateProgress(key);			
+			}
+		}
 	}
-	public JSONObject getCredentials() throws IOException{
+	//this is a clone of the method found in ConfigResource.java
+	public static JSONObject getCredentials() throws IOException{
 		JSONObject responseObject=new JSONObject();
 		JSONArray responseArray=new JSONArray();
 		try{
@@ -52,7 +79,8 @@ public class ScheduleService {
 		responseObject.put("users", responseArray);
 		return responseObject;
 	}
-	public void sendMail(){
-		NotificationService nService=new NotificationService("to", "subject", "text");
+	//This method sends an alert notification
+	public static void sendMail(String m_to, String body){
+		NotificationService nService=new NotificationService(m_to, "PM-Portal Alert", body);
 	}
 }
