c31219fb82947644618f05ede3f392d885ff3146
Aman <Aman@Aman-PC>
2016-08-04 11:25:59 -0600

    Fixed strange git changes

--- pmportal/src/main/java/com/sgt/pmportal/resource/ConfigResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/ConfigResource.java
@@ -66,13 +66,9 @@ public class ConfigResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/get")
-<<<<<<< HEAD
-	//Retrieves all the information in the config file as a JSON string except passwords
-	public String getAllCredentials() throws IOException{
-=======
+
 	//Retrieves all the information in the config file as a JSON string except passwords
 	public static String getAllCredentials() throws IOException{
->>>>>>> origin/master
 		JSONObject responseObject=new JSONObject();
 		JSONArray responseArray=new JSONArray();
 		String fileString;
@@ -103,40 +99,6 @@ public class ConfigResource {
 		responseObject.put("users", responseArray);
 		return responseObject.toString();
 	}
-<<<<<<< HEAD
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/get/user/{username}")
-	//Retrieves information on an individual user
-	public String getUserCredentials(@PathParam ("username") String username) throws IOException{
-		JSONObject responseObject=new JSONObject();
-		String fileString;
-		try{
-			//Tomcat
-			fileString=new String(Files.readAllBytes(Paths.get("webapps/pmportal/data/config.txt")), StandardCharsets.UTF_8);
-		}catch (Exception e){
-			//glassfish
-			fileString=new String(Files.readAllBytes(Paths.get("../applications/pmportal/data/config.txt")), StandardCharsets.UTF_8);
-		}
-		if (fileString.toLowerCase().contains(username.toLowerCase())){
-			int startIndex=fileString.indexOf(username);
-			//do not add 1 to length or else will include semicolon
-			int length=fileString.substring(startIndex).indexOf(";");
-			int finalIndex=startIndex+length;
-			String userString=fileString.substring(startIndex, finalIndex);
-			String[] userData=userString.split(",");
-			responseObject.put("username", userData[0]);
-			responseObject.put("email", userData[2]);
-			responseObject.put("url", userData[3]);
-			responseObject.put("seaMin", userData[4]);
-			responseObject.put("seaMax", userData[5]);
-			responseObject.put("eeaMin", userData[6]);
-			responseObject.put("eeaMax", userData[7]);
-			responseObject.put("bugMax", userData[8]);
-		}
-		return responseObject.toString();
-	}
-=======
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/get/user/{username}")
@@ -203,5 +165,4 @@ public class ConfigResource {
 		responseObject.put("users", responseArray);
 		return responseObject;
 	}
->>>>>>> origin/master
 }
--- pmportal/src/main/java/com/sgt/pmportal/services/AlertService.java
+++ pmportal/src/main/java/com/sgt/pmportal/services/AlertService.java
@@ -16,63 +16,10 @@ public class AlertService {
 
 	//This could literally take an hour. This method should only be run on a weekly basis, if that
 	public static void checkMetrics() throws IOException, URISyntaxException, ParseException{
-		JSONObject userObject=new JSONObject(ConfigResource.getUsersUnexposed());
+		JSONObject userObject=new JSONObject(ConfigResource.getAllCredentials());
 		JSONArray userArray=userObject.getJSONArray("users");
 		for (int i=0; i<userArray.length(); i++){
 			JSONObject user=userArray.getJSONObject(i);
-<<<<<<< HEAD
-			String username=user.getString("username");
-			String password=user.getString("password");
-			String url=user.getString("url");
-			JiraRestClient client=GeneralServices.login(url, username, password);
-			String authorization=GeneralServices.encodeAuth(username, password);
-			ProjectServices projectService=new ProjectServices(client, authorization, url);
-			List<JiraProject> projectList=projectService.getAllJiraProjects();
-			MetricsServices metricService=new MetricsServices(client, authorization, url);
-			for (JiraProject project:projectList){
-				String key=project.getKey();
-				Double sea=metricService.calculateProjectSEA(project, null);
-				Double eea=metricService.calculateProjectEEA(project, null);
-				Long bugs=metricService.calculateBugs(key);
-
-			}
-		}
-	}
-	//this is a clone of the method found in ConfigResource.java
-	public static JSONObject getCredentials() throws IOException{
-		JSONObject responseObject=new JSONObject();
-		JSONArray responseArray=new JSONArray();
-		try{
-			//Tomcat
-			//read file
-			String fileString=new String(Files.readAllBytes(Paths.get("webapps/pmportal/data/config.txt")), StandardCharsets.UTF_8);
-			//convert to JSON so it can be easily manipulated client-side
-			String[] userArray=fileString.split(";");
-
-			for (String user:userArray){
-				String[] userData=user.split(",");
-				if (userData.length>1){
-					JSONObject tempObject=new JSONObject();
-					tempObject.put("username", userData[0]);
-					tempObject.put("email", userData[2]);
-					tempObject.put("url", userData[3]);
-					responseArray.put(tempObject);
-				}
-			}
-		}catch (Exception e){
-			//glassfish
-			String fileString=new String(Files.readAllBytes(Paths.get("../applications/pmportal/data/config.txt")), StandardCharsets.UTF_8);
-			String[] userArray=fileString.split(";");
-
-			for (String user:userArray){
-				String[] userData=user.split(",");
-				if (userData.length>1){
-					JSONObject tempObject=new JSONObject();
-					tempObject.put("username", userData[0]);
-					tempObject.put("email", userData[2]);
-					tempObject.put("url", userData[3]);
-					responseArray.put(tempObject);
-=======
 			String email=user.getString("email");
 			if (email!=null && email!=""){
 				String username=user.getString("username");
@@ -109,7 +56,6 @@ public class AlertService {
 								+ "The accepted maximum was " +bugMax+ ", but the Bug count is "+bugs+".";
 						sendMail(email,body);
 					};
->>>>>>> origin/master
 				}
 			}
 		}
