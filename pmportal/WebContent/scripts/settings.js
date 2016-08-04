69ccd4d83b4f59e1b476665a4bd58c981441d862
Aman <Aman@Aman-PC>
2016-08-04 09:42:20 -0600

    Added setting to set boundaries for notifications, added settings to config post

--- pmportal/WebContent/scripts/settings.js
+++ pmportal/WebContent/scripts/settings.js
@@ -11,8 +11,12 @@ function saveSettings(){
 	var user=$("#userText").val();
 	var baseURL=$("#urlText").val();
 	var pass=$("#passText").val();
+	var seaMin=$("#seaMin").val();
+	var seaMax=$("#seaMax").val();
+	var eeaMin=$("$eeaMin").val();
+	var eeaMax=$("#eeaMax").val();
+	var bugMax=$("#bugMax").val();
 	var remember=$("#rememberBox").prop("checked");
-
 	var testResource="http://"+hostURL+"/pmportal/rest/test/login/" + user + "/" + pass + "/" +baseURL;
 	$.ajax({
 		type:"GET",
@@ -33,8 +37,8 @@ function saveSettings(){
 };
 });
 };
-function saveToConfig(user, pass, baseURL, eaddress){
-	var request="{\"username\":\"" + user+"\", \"password\":\""+ pass + "\", \"url\":\"" + baseURL+"\", \"email\":\"" + eaddress+"\"}";
+function saveToConfig(user, pass, baseURL, eaddress, seaMin, seaMax, eeaMin, eeaMax, bugMax){
+	var request="{\"username\":\"" + user+"\", \"password\":\""+ pass + "\", \"url\":\"" + baseURL+"\", \"email\":\"" + eaddress+"\", \"seaMin\":\""+seaMin+"\", \"seaMax\":\""+seaMax+"\", \"eeaMin\":\""+eeaMin+"\", \"eeaMax\":\""+eeaMax+"\", \"bugMax\":\""+bugMax+"\"}";
 	var resource="http://"+hostURL+"/pmportal/rest/config/save";
 	$.ajax({
 		type:"POST",
@@ -68,4 +72,15 @@ function testEmail(){
 			alert("Email failed to send!");
 };
 });
+};
+function resetSEA(){
+	$("#seaMin").val("0.8");
+	$("#seaMax").val("1.25");
+};
+function resetEEA(){
+	$("#eeaMin").val("0.8");
+	$("#eeaMax").val("1.25");
+};
+function resetBugs(){
+	$("#bugMax").val("10");
 };
