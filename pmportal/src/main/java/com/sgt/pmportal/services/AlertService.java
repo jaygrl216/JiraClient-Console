f15f318c5cee39084cada870b50d0ce8ce201d75
Aman <Aman@Aman-PC>
2016-08-04 10:15:49 -0600

    Second alert

--- pmportal/src/main/java/com/sgt/pmportal/services/AlertService.java
+++ pmportal/src/main/java/com/sgt/pmportal/services/AlertService.java
@@ -43,10 +43,15 @@ public class AlertService {
 				Double eea=metricService.calculateProjectEEA(project, null);
 				Long bugs=metricService.calculateBugs(key);
 				if (sea<seaMin || sea>seaMax){
-					String body="Your SEA value for project \""+projectName+"\" has exceeded the accepted limitations. "
+					String body="Your Schedule Estimation Accuracy value for project \""+projectName+"\" has exceeded the accepted limitations. "
 							+ "The accepted range was from " +seaMin + " to " +seaMax + ", but the SEA value is "+sea+".";
 					sendMail(user.getString("email"),body);
-				}
+				};
+				if (eea<eeaMin || eea>eeaMax){
+					String body="Your Effort Estimation Accuracy value for project \""+projectName+"\" has exceeded the accepted limitations. "
+							+ "The accepted range was from " +eeaMin + " to " +eeaMax + ", but the EEA value is "+eea+".";
+					sendMail(user.getString("email"),body);
+				};
 			}
 		}
 	}
