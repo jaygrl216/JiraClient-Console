171223ff246314ae19330452307a85812759c4d8
Aman <Aman@Aman-PC>
2016-08-10 15:09:48 -0600

    Added abstract user login

--- pmportal/WebContent/scripts/select.js
+++ pmportal/WebContent/scripts/select.js
@@ -24,3 +24,33 @@ $.ajax({
 		$("#userList").append("<li>" + user.username + ", " + user.alias+"</li>");
 	});
 });
+$(document).ready(function(){
+	$("#userList").on( "click", "li" , function () {
+		var item = $(this).text();
+		$.each(userArray, function (index, user) {
+			if(user.username + ", " + user.alias == item) {
+				selectUser(user.username);
+			};
+		});
+	});
+});
+function selectUser(username){
+	var configResource="http://" + hostURL+ "/pmportal/rest/config/get/user/" + pm+"/"+username;
+	$.ajax({
+		type : "GET",
+		dataType : "text",
+		url : configResource
+	}).fail(function(xhr, status, errorThrown) {
+		console.log("Error: " + errorThrown);
+		console.log("Status: " + status);
+		console.dir(xhr);
+		alert("Failed to contact server!");
+	}).done(function(jsonObject){
+		responseObject=jsonObject;
+		var uname=responseObject.username;
+		var pass=responseObject.password;
+		var url=responseObject.url;
+		setCookie(uname, pass, url, pm);
+		window.location="home.html";
+	});
+};
