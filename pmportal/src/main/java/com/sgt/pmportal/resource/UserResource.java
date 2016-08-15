0186b440fb67dde0171062bd76914194c1ba9218
Aman <Aman@Aman-PC>
2016-08-15 11:32:09 -0600

    Added method in UserResource to get user allocation

--- pmportal/src/main/java/com/sgt/pmportal/resource/UserResource.java
+++ pmportal/src/main/java/com/sgt/pmportal/resource/UserResource.java
@@ -1,29 +1,47 @@
 package com.sgt.pmportal.resource;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path ("/user/{username}/{password}/{url:.+}")
+import org.json.JSONObject;
+
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.sgt.pmportal.domain.JiraProject;
+import com.sgt.pmportal.services.GeneralServices;
+import com.sgt.pmportal.services.ProjectServices;
+import com.sgt.pmportal.services.SprintServices;
+
+@Path ("/user/{key}/{username}/{password}/{url:.+}")
 public class UserResource {
 
 	
 	/**
-	 * This method will return the user's information as well as issues they are 
-	 * assigned to.
+	 * This method will return the resource allocation of each user
 	 * 
 	 * @param username
 	 * @param password
 	 * @return
 	 * @throws URISyntaxException
+	 * @throws ParseException 
+	 * @throws IOException 
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getProjects(@PathParam ("username") String username, 
-			@PathParam ("password")	String password, @PathParam ("url") String url) throws URISyntaxException{
-		return "";
+	public String getResourceAllocation(@PathParam ("key") String projectKey, @PathParam ("username") String username, 
+			@PathParam ("password")	String password, @PathParam ("url") String url) throws URISyntaxException, IOException, ParseException{
+		JiraRestClient client=GeneralServices.login(url, username, password);
+		String authorization=GeneralServices.encodeAuth(username, password);
+		ProjectServices projectService=new ProjectServices(client, authorization, url);
+		SprintServices sprintService=new SprintServices(client, authorization, url);
+		JiraProject project=projectService.getProjectByKey(projectKey);
+		JSONObject returnObject=sprintService.resourceAllocation(project);
+		return returnObject.toString();
 	}
 }
